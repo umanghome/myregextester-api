e24244b9cc039aefa02896dfb1babda377820176
Umang Galaiya <umang.galaiya@razorpay.com>
2019-06-19 21:38:15 +0530

    Remove logs added for testing

--- functions/server.js
+++ functions/server.js
@@ -63,8 +63,7 @@ app.post('/optimize', (request, response) => {
   } = request.body;
 
   if (!regex) {
-    console.log(request.body);
-    return response.status(400).send(createError('MISSING_PARAMS', JSON.stringify(request.body)));
+    return response.status(400).send(createError('MISSING_PARAMS'));
   }
 
   Request({
